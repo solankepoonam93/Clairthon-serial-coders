ceff6504e37188a36292df9d9fd6e61871a2187a
bhushankarmarkar <v-bhushan.karmarkar@shutterfly.com>
2022-09-20 14:20:56 +0530

    Persist is working now.

--- src/main/java/com/cv/sc/web/controller/impl/EntityController.java
+++ src/main/java/com/cv/sc/web/controller/impl/EntityController.java
@@ -3,7 +3,10 @@ package com.cv.sc.web.controller.impl;
 import com.cv.sc.model.SCEntity;
 import com.cv.sc.storage.StorageService;
 import com.cv.sc.storage.impl.DBStorageServiceImpl;
+import com.cv.sc.web.controller.SCController;
 import com.cv.sc.web.utill.EntityTypes;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,7 +19,7 @@ import java.io.UnsupportedEncodingException;
  */
 @RestController
 @RequestMapping("/dao")
-public class EntityController<T extends SCEntity> implements SCEntity {
+public class EntityController<T extends SCEntity> implements SCController {
 
     private StorageService dbStorageService;
 
@@ -24,10 +27,13 @@ public class EntityController<T extends SCEntity> implements SCEntity {
         this.dbStorageService = new DBStorageServiceImpl();
     }
 
-    @PostMapping(path = "/persist", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public T persist(T entity) throws UnsupportedEncodingException {
-        entity = (T) dbStorageService.save(entity);
-        return entity;
+    @PostMapping(path = "/persist/{entityName}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public T persist(@PathVariable String entityName, @RequestBody String entityJson) throws UnsupportedEncodingException, JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Class<T> clazz = EntityTypes.getEntityClass(entityName);
+        T t = objectMapper.readValue(entityJson, clazz);
+        t = (T) dbStorageService.save(t);
+        return t;
     }
 
     @GetMapping(path = "/fetch/{entityName}/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
