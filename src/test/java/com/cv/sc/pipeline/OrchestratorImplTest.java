bcd5417d4cbd872c8fda2de5157d1db9a6da661f
bhushankarmarkar <v-bhushan.karmarkar@shutterfly.com>
2022-09-26 15:37:31 +0530

    Corrected search terms inside search configuration

--- src/test/java/com/cv/sc/pipeline/OrchestratorImplTest.java
+++ src/test/java/com/cv/sc/pipeline/OrchestratorImplTest.java
@@ -2,16 +2,12 @@ package com.cv.sc.pipeline;
 
 import com.cv.sc.model.Config;
 import com.cv.sc.model.SearchResponse;
-import com.cv.sc.model.github.GitHubEntity;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.List;
-import java.util.Map;
-
 public class OrchestratorImplTest {
 
-    OrchestratorImpl orchestrator = new OrchestratorImpl();
+    private Orchestrator orchestrator = new OrchestratorImpl();
     @Test
     public void searchTest() throws Exception {
         Config config = getConfig();
@@ -19,6 +15,7 @@ public class OrchestratorImplTest {
         Assert.assertTrue(searchResponse.getContentSearchResults().size()>0);
         Assert.assertTrue(searchResponse.getUserSearchResults().size()>0);
         Assert.assertTrue(searchResponse.getFileSearchResults().size()>0);
+        // System.out.println(new ObjectMapper().writeValueAsString(searchResponse));
     }
 
     private Config getConfig() {
@@ -26,10 +23,10 @@ public class OrchestratorImplTest {
         config.setId(1L);
         config.setConfigName("test1");
         config.setCodeSearchKeywords(new String[]{"CountAboveSixty"});
-        config.setCustomerName("Amruta");
+        config.setCustomerName("TestCustomer1");
         config.setUserSearchKeywords(new String[]{"AmrutaChichani"});
-        config.setFileNames(new String[] {"clairthon-serial-coders"});
-        config.setRepositoryNames(new String[]{"AmrutaChichani/Java_8_demo_project"});
+        config.setFileNames(new String[] {"OrchestratorImplTest"});
+        config.setRepositoryNames(new String[]{"clairthon-serial-coders"});
         return config;
     }
 }
